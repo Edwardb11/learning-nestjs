4467e1d9536636aff56f7c00a9087a7c4bbe1a27
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-21 11:49:04 -0400

    agregando modulo y servicio a las tareas

--- intro-nestjs/src/app.module.ts
+++ intro-nestjs/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TasksController } from './tasks/tasks.controller';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TasksController],
+  imports: [TasksModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
