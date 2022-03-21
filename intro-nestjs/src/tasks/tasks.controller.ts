67f86b6b51830c7f94f155523fc7286c7dc12ae3
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-21 10:32:59 -0400

    agregando las demas rutas de los metodos basicos

--- intro-nestjs/src/tasks/tasks.controller.ts
+++ intro-nestjs/src/tasks/tasks.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
   //  decorador Get
   @Get()
   // ruta atravez del metodo get y funciona atravez del metodo que esta abajo
-  getTasks() {
-    return 'tasks';
+  getTasks(): string {
+    return 'Obteniendo una tarea';
+  }
+  @Post()
+  createTasks(): string {
+    return 'Creando una tarea';
+  }
+  @Put()
+  updateTasks(): string {
+    return 'Actualizando una tarea';
+  }
+  @Delete()
+  deleteTasks(): string {
+    return 'Eliminando una tarea';
   }
 }
