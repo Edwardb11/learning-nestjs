598860a98526a440a65f22057c7506673b874a7f
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-22 11:01:01 -0400

    agregando las validaciones

--- crud-mysql/src/post/dtos/create-post.dto.ts
+++ crud-mysql/src/post/dtos/create-post.dto.ts
@@ -1,11 +1,18 @@
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { PostCategory } from '../enums/post-category.enum';
-
+// https://github.com/typestack/class-validator
 export class CreatePostDto {
+  @IsString()
   title: string;
   slug: string;
+  @IsString()
   excerpt: string;
+  @IsString()
   content: string;
+  @IsString()
   category: PostCategory;
+  @IsString()
   tags: string[];
+  @IsBoolean()
   status: boolean;
 }
