e404afa0ba5e7134af2ed8d29beff5578dc8ebc0
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-03-24 11:34:57 -0400

    actualizando el puerto

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   logger.log(`El servidor esta corriendo en ${await app.getUrl()}`);
 }
 bootstrap();
