a0f4c2829806ed31894f8d5604c5978b6b3da36f
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-13 14:33:42 -0400

    feat: service for create post and DTO

--- mysql-typeorm/src/posts/posts.service.ts
+++ mysql-typeorm/src/posts/posts.service.ts
@@ -1,7 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './post.entity';
+import { Repository } from 'typeorm';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
 export class PostsService {
-  createPost() {}
-  getPost() {}
+  constructor(
+    @InjectRepository(Post) private postRepository: Repository<Post>,
+    private userSerivice: UsersService,
+  ) {}
+  async createPost(post: CreatePostDto) {
+    const userFound = await this.userSerivice.getUser(post.authorId);
+    if (!userFound) {
+      return new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+    const newPost = this.postRepository.create(post);
+    return this.postRepository.save(newPost);
+  }
+
+  getPost() {
+    return this.postRepository.find();
+  }
 }
