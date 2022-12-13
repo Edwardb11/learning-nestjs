dfea4c305b3c9ee720f0e34f4f9429ddc0c09e3b
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-13 10:50:01 -0400

    feat: enpoint for create new user

--- mysql-typeorm/src/users/users.controller.ts
+++ mysql-typeorm/src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-ser.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Post()
+  createUser(@Body() newUser: CreateUserDto) {
+    return this.usersService.createUser(newUser);
+  }
+}
--- mysql-typeorm/src/users/users.service.ts
+++ mysql-typeorm/src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-ser.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  createUser(user: CreateUserDto) {
+    const newUser = this.userRepository.create(user);
+    return this.userRepository.save(newUser);
+  }
+}
