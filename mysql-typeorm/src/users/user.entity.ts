d8ea27adb27893acdc6b21f454a13d93e0c3d97e
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-13 10:34:55 -0400

    feat: user entity

---
+++ mysql-typeorm/src/users/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  CreateAt: Date;
+
+  @Column()
+  authStrategy: string;
+}
