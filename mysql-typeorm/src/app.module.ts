c1f0d1959bb75e6d4cd905843890df966e3ff724
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-13 10:13:56 -0400

    feat: install and configure nestjs

--- mysql-typeorm/src/app.module.ts
+++ mysql-typeorm/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'projectNestjs',
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
