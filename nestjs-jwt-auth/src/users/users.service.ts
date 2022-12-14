16f4ee181d384450b20d286676a91ce02a3ccc4a
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2022-12-14 14:04:49 -0400

    setting up service users

--- nestjs-jwt-auth/src/users/users.service.ts
+++ nestjs-jwt-auth/src/users/users.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  private users: User[] = [
+    {
+      id: 0,
+      name: 'Bob',
+      email: 'bob@gmail.com',
+      password: 'bobPass',
+    },
+
+    {
+      id: 1,
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'johnPass',
+    },
+
+    {
+      id: 2,
+      name: 'Gary',
+      email: 'gary@gmail.com',
+      password: 'garyPass',
+    },
+  ];
+  findByEmail(email: string): Promise<User | undefined> {
+    const user = this.users.find((user) => user.email === email);
+    if (user) {
+      return Promise.resolve(user);
+    }
+    return undefined;
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  findOne(id: number): Promise<User | undefined> {
+    const user = this.users.find((user) => user.id === id);
+    if (user) {
+      return Promise.resolve(user);
+    }
+    return undefined;
   }
 }
