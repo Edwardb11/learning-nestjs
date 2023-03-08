628d0d59323636eddf88ad3f23caa2e2bb897c6b
Edwardb11 <edwardbrito11@hotmail.com>
2023-03-08 15:12:26 -0400

    RES API

---
+++ store/src/controllers/products.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+  HttpStatus,
+  HttpCode,
+  Res,
+} from '@nestjs/common';
+
+import { Response } from 'express';
+
+@Controller('products')
+export class ProductsController {
+  @Get()
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return {
+      message: `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
+    };
+  }
+
+  @Get('filter')
+  getProductFilter() {
+    return `yo soy un filter`;
+  }
+
+  @Get(':productId')
+  @HttpCode(HttpStatus.ACCEPTED)
+  getOne(@Res() response: Response, @Param('productId') productId: string) {
+    response.status(200).send({
+      message: `product ${productId}`,
+    });
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'accion de crear',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return id;
+  }
+}
