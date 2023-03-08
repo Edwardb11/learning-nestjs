924845e84f9cfc78d8bc7db685c3eb12421ea838
Edwardb11 <edwardbrito11@hotmail.com>
2023-03-08 15:44:08 -0400

    feat: implements services

---
+++ store/src/services/products.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from 'src/entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  private counterId = 1;
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'bla bla',
+      price: 122,
+      image: '',
+      stock: 12,
+    },
+  ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  create(payload: any) {
+    this.counterId = this.counterId + 1;
+    const newProduct = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  update(id: number, payload: any) {
+    const product = this.findOne(id);
+    if (product) {
+      const index = this.products.findIndex((item) => item.id === id);
+      this.products[index] = {
+        ...product,
+        ...payload,
+      };
+      return this.products[index];
+    }
+    return null;
+  }
+}
