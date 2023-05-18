d9e5675fffb10ea92ac1ca934868b5e021754435
Edwardb11 <edwardbrito11@hotmail.com>
2023-05-18 11:13:55 -0400

    feat: module config and config env NODE_ENV= .env

--- store-modular/src/app.module.ts
+++ store-modular/src/app.module.ts
@@ -6,19 +6,30 @@ import { UsersModule } from './users/users.module';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { enviroments } from 'enviroments';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
 
 @Module({
-  imports: [HttpModule, UsersModule, ProductsModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      isGlobal: true,
+    }),
+    HttpModule,
+    UsersModule,
+    ProductsModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
+    // {
+    //   provide: 'API_KEY',
+    //   useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    // },
     {
       provide: 'TASKS',
       useFactory: async (http: HttpService) => {
--- store-modular/src/users/service/users.service.ts
+++ store-modular/src/users/service/users.service.ts
@@ -4,10 +4,14 @@ import { User } from '../entities/user.entity';
 import { CreateUserDto, UpdateUserDto } from '../dtos/user.dto';
 import { ProductsService } from 'src/products/services/products.service';
 import { Order } from '../entities/order.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
-  constructor(private productsService: ProductsService) {}
+  constructor(
+    private productsService: ProductsService,
+    private configService: ConfigService,
+  ) {}
 
   private counterId = 1;
   private users: User[] = [
@@ -20,6 +24,9 @@ export class UsersService {
   ];
 
   findAll() {
+    const apiKey = this.configService.get('API_KEY');
+    const dbName = this.configService.get('DATABASE_NAME');
+    console.log(apiKey, dbName);
     return this.users;
   }
 
