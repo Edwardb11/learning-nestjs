8a26bd15a4f51077a2fa3a873c527a95f7c6d865
Edwardb11 <edwardbrito11@hotmail.com>
2023-05-19 10:12:11 -0400

    validation .env

--- store-modular/src/app.module.ts
+++ store-modular/src/app.module.ts
@@ -9,6 +9,7 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { enviroments } from 'enviroments';
 import config from '../config';
+import * as Joi from 'joi';
 
 // const API_KEY = '12345634';
 // const API_KEY_PROD = 'PROD1212121SA';
@@ -17,8 +18,13 @@ import config from '../config';
   imports: [
     ConfigModule.forRoot({
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
-      isGlobal: true,
       load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     HttpModule,
     UsersModule,
